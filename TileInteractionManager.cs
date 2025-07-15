70d63969046091a29dbb62165e0cb54b242b4a08
Prashanna Kandel <40130947+PrashKandel63@users.noreply.github.com>
2025-07-15 17:42:00 -0400

    VS solution file and Tile Map code incomplete.

---
+++ TileInteractionManager.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileInteractionManager : MonoBahaviour
+{
+	public Tilemap groundTilemap; // Assign the ground tilemap in the Inspe
+	{
+	}
+}
