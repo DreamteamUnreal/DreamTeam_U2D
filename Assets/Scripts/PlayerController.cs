491460fc00b61aa55aece867d19a5aebae44430e
lostpromise0710 <sonder0710@example.com>
2025-07-15 20:51:54 -0400

    init the file structure

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBhaviour
+{
+	public float moveSpeed = 5f;
+	private Rigidbody2D rb;
+	private Vector2 moveInput;
+
+	public InventoryManager inventoryManager; //Assign in Inspector
+	public CraftingManager craftingManager; //Assign in Inspector
+
+	private Interactable currentInteractable;
+
+	void Awake()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	public void OnMove(InputAction.CallbackContext context)
+	{
+		moveInput = context.ReadValue<Vector2>();
+	}
+    
+	public void OnInteract(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+		{
+			if (currentInteractable != null)
+			{
+				currentInteractable.Interact(this.gameObject);
+			}
+			else {
+			}
+		}
+    }
+
+    public void OnUseSpecialFeature(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+		{
+			Debug.Log("Special Feature Used!");
+		}
+    }
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		Interactable interactable = other.GetComponent<Interactable>();
+		if (interactable != null)
+		{
+			currentInteractable = interactable;
+		}
+	}
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Interactable interactable = other.GetComponent<Interactable>();
+        if (interactable != null && currentInteractable == interactable)
+        {
+            currentInteractable = null;
+        }
+    }
+}
---
+++ Assets/Scripts/TileInteractionManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileInteractionManager : MonoBahaviour
+{
+	public Tilemap groundTilemap; //Assign the ground tilemap in the Inspector
+	public Tlemap interactableTilemap; //For tings like berries and other stuff like that
+	public Tilemap obstacleTilemap; //For unpassable objects like fances and trees
+
+	public TileBase scarecrowTile;
+	public TileBase bushShowTile;
+	public TileBase riverTile;
+	public TileBase bridgeTile;
+
+	public Vector3Int WorldToCall(Vector3 worldPosition)
+	{
+		if(groundTilemap != null)
+		{
+			return groundTilemap.WorldToCall(worldPosition);
+		}
+		return Vector3.zero;
+	}
+
+	public bool IsTileOfType(Vector3Int callPosition, Tilemap tragetTilemap, TileBase tragetTile)
+	{
+		if (tragetTilemap != null)
+		{
+			return tragetTilemap.GetTile(callPosition) == tragetTile;
+		}
+		return false;
+	}
+	public bool IsCallPossable(Vector3Int callPosition) {
+		if (obstacleTilemap != null && obstacleTilemap.HasTile(callPosition))
+		{
+			return false; //Call has an obstacle
+		}
+		//Add checks for other non-passable tiles if they are on different layers
+		return true;
+	}
+ }
