c97db04769d40e983677a02049f78f362e2a85f6
Prashanna Kandel <40130947+PrashKandel63@users.noreply.github.com>
2025-07-27 22:32:06 -0400

    Bug Fixes

--- Assets/Scripts/UI/GameOverUI.cs
+++ Assets/Scripts/UI/GameOverUI.cs
@@ -4,75 +4,77 @@ using UnityEngine.SceneManagement;
 
 public class GameOverUI : MonoBehaviour
 {
-    public GameObject normalHighScoreUI;
-    public GameObject lostUIPanel;
-    public TMP_Text finalScoreText;
-    public TMP_Text highScoreText;
-    public TMP_Text totalPiesText;
+	public GameObject normalHighScoreUI;
+	public GameObject lostUIPanel;
+	public TMP_Text finalScoreText;
+	public TMP_Text highScoreText;
+	public TMP_Text totalPiesText;
 
-    private void Start()
-    {
-        if (lostUIPanel != null)
-        {
-            lostUIPanel.SetActive(false);
-        }
-    }
+	private void Start()
+	{
+		if (lostUIPanel != null)
+		{
+			lostUIPanel.SetActive(false);
+		}
+	}
 
-    public void ShowGameOver(int finalScore, int highScore, int totalPies)
-    {
-        if (lostUIPanel != null)
-        {
-            lostUIPanel.SetActive(true);
+	public void ShowGameOver(int finalScore, int highScore, int totalPies)
+	{
+		if (lostUIPanel != null)
+		{
+			lostUIPanel.SetActive(true);
 
-            if (normalHighScoreUI != null)
-                normalHighScoreUI.SetActive(false); // Hides the HUD UI
+			if (normalHighScoreUI != null)
+			{
+				normalHighScoreUI.SetActive(false); // Hides the HUD UI
+			}
 
-            if (finalScoreText != null)
-            {
-                finalScoreText.text = $"Final Score: {finalScore}";
-            }
+			if (finalScoreText != null)
+			{
+				finalScoreText.text = $"Final Score: {finalScore}";
+			}
 
-            if (highScoreText != null)
-            {
-                highScoreText.text = $"High Score: {highScore}";
-            }
+			if (highScoreText != null)
+			{
+				highScoreText.text = $"High Score: {highScore}";
+			}
 
-            if (totalPiesText != null)
-            {
-                totalPiesText.text = $"Total Pies Made: {totalPies}";
-            }
-        }
-    }
+			if (totalPiesText != null)
+			{
+				totalPiesText.text = $"Total Pies Made: {totalPies}";
+			}
+		}
+	}
 
-    public void OnRestartButton()
-    {
-        Scene currentScene = SceneManager.GetActiveScene();
+	public void OnRestartButton()
+	{
+		Scene currentScene = SceneManager.GetActiveScene();
 
-        if (normalHighScoreUI != null)
-        {
-            normalHighScoreUI.SetActive(true); // Show HUD again
-        }
+		if (normalHighScoreUI != null)
+		{
+			normalHighScoreUI.SetActive(true); // Show HUD again
+		}
 
-        if (currentScene.name == "Lvl_L1")
-        {
-            PlayerData.Instance.ResetAll();
-            SceneManager.LoadScene("Lvl_L1");
-        }
-        else
-        {
-            SceneManager.LoadScene(currentScene.buildIndex);
-        }
-    }
+		if (currentScene.name == "Lvl_L1")
+		{
+			PlayerData.Instance.ResetAll();
+			SceneManager.LoadScene("Lvl_L1");
+		}
+		else
+		{
+			SceneManager.LoadScene(currentScene.buildIndex);
+		}
+	}
 
-    public void OnMainMenu()
-    {
-        SceneManager.LoadScene("MainMenu");
-    }
-    public void OnQuitButton()
-    {
-        Application.Quit();
+	public void OnMainMenu()
+	{
+		SceneManager.LoadScene("MainMenu");
+	}
+	public void OnQuitButton()
+	{
+		Application.Quit();
 #if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false; // For stopping Play mode in Unity Editor
+		UnityEditor.EditorApplication.isPlaying = false; // For stopping Play mode in Unity Editor
 #endif
-    }
+	}
 }
